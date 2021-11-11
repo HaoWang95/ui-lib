c13d724d8be6eece13ffa6127ea51d9bdffc3377
HaoWang95 <alanwang0028@gmail.com>
2021-11-11 11:26:49 +1100

    readme cleanup

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -7,9 +7,10 @@ export default {
 }
 
 export const PrimaryButton = () => <Button variant="primary">Primary</Button>;
+PrimaryButton.storyName = 'A Primary Button example';
 
 export const SecondaryButton = () => <Button variant="secondary">Secondary</Button>;
 
-export const Success = () => <Button variant="success">Success</Button>
+export const Success = () => <Button variant="success">Success</Button>;
 
-export const Danger = () => <Button variant="danger">Danger</Button>
+export const Danger = () => <Button variant="danger">Danger</Button>;
